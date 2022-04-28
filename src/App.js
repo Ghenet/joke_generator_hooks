b9b90d02723ca1b6785bf1d48b307e80fa0b6156
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2022-04-28 13:31:21 -0700

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+const API_URL = `http://api.icndb.com/jokes/random?firstName=Ghenet&lastName=Kibrom`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <h1>Chuck Norris Joke Generator</h1>
+      <p>Here goes the Joke</p>
+      <button>Get new joke</button>
     </div>
   );
 }
