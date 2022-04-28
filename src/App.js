5effdf9cda0d080fdaff6be79c0b127152d6ea83
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2022-04-28 14:29:27 -0700

    uses useEffect hook to get jokes from api and sets the joke

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React, {useState, useEffect} from 'react';
 
 const API_URL = `http://api.icndb.com/jokes/random?firstName=Ghenet&lastName=Kibrom`
 
 function App() {
+  const [joke, setJoke] = useState('');
+
+  useEffect(()=> {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setJoke(data.value.joke));
+  },[])
+
   return (
     <div className="box">
-      <h1>Chuck Norris Joke Generator</h1>
-      <p>Here goes the Joke</p>
+      <h3>Chuck Norris Joke Generator</h3>
+      <p>{joke}</p>
       <button>Get new joke</button>
     </div>
   );
